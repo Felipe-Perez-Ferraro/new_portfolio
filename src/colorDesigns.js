d370e6e89b7a253055a178bc2484f82a7354b9c7
Felipe <fperezferraro@gmail.com>
2024-01-08 21:31:16 -0300

    Runned prettier

--- src/colorDesigns.js
+++ src/colorDesigns.js
@@ -1,27 +1,26 @@
-export const colorDesigns = 
-  {
-    designOne: {
-      background: 'bg-sky-950',
-      title: 'text-sky-700',
-      subtitle: 'text-teal-400',
-      paragraph: 'text-amber-400',
-      text: 'text-teal-100',
-      border: 'border-amber-400'
-    },
-    designTwo: {
-      background: 'bg-orange-100',
-      title: 'text-amber-400',
-      subtitle: 'text-orange-600',
-      paragraph: 'text-orange-950',
-      text: 'text-amber-800',
-      border: 'border-orange-950'
-    },
-    designThree: {
-      background: 'bg-violet-950',
-      title: 'text-purple-700',
-      subtitle: 'text-violet-300',
-      paragraph: 'text-purple-600',
-      text: 'text-purple-100',
-      border: 'border-purple-600'
-    },
-  };
+export const colorDesigns = {
+  designOne: {
+    background: 'bg-sky-950',
+    title: 'text-sky-700',
+    subtitle: 'text-teal-400',
+    paragraph: 'text-amber-400',
+    text: 'text-teal-100',
+    border: 'border-amber-400',
+  },
+  designTwo: {
+    background: 'bg-orange-100',
+    title: 'text-amber-400',
+    subtitle: 'text-orange-600',
+    paragraph: 'text-orange-950',
+    text: 'text-amber-800',
+    border: 'border-orange-950',
+  },
+  designThree: {
+    background: 'bg-violet-950',
+    title: 'text-purple-700',
+    subtitle: 'text-violet-300',
+    paragraph: 'text-purple-600',
+    text: 'text-purple-100',
+    border: 'border-purple-600',
+  },
+};
