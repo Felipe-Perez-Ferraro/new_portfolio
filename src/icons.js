8958b5787741409319f3041513e95432ffc7e02c
Felipe <fperezferraro@gmail.com>
2024-01-09 16:14:56 -0300

    Updated icons file

--- src/icons.js
+++ src/icons.js
@@ -1,6 +1,8 @@
 import { faGithub, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const linkedin = <FontAwesomeIcon icon={faLinkedin} />;
 export const github = <FontAwesomeIcon icon={faGithub} />;
 export const instagram = <FontAwesomeIcon icon={faInstagram} />;
+export const leftArrow = <FontAwesomeIcon icon={faChevronLeft} />;
