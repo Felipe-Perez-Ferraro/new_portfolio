6d168806b63798b45782ba7ac24cd4cea3b6488e
Felipe <fperezferraro@gmail.com>
2024-01-08 21:31:22 -0300

    Created icons file

---
+++ src/icons.js
@@ -0,0 +1,6 @@
+import { faGithub, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export const linkedin = <FontAwesomeIcon icon={faLinkedin} />;
+export const github = <FontAwesomeIcon icon={faGithub} />;
+export const instagram = <FontAwesomeIcon icon={faInstagram} />;
