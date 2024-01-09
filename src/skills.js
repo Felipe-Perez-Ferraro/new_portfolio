e26d5c582b044b3940562453b987691d99d4edc4
Felipe <fperezferraro@gmail.com>
2024-01-09 18:51:16 -0300

    Created skills file

---
+++ src/skills.js
@@ -0,0 +1,10 @@
+const skills = [
+  { id: 1, skill: 'Pair Programming' },
+  { id: 2, skill: 'Advanced English' },
+  { id: 3, skill: 'React Redux' },
+  { id: 4, skill: 'Tailwind CSS' },
+  { id: 5, skill: 'Databases' },
+  { id: 6, skill: 'API Management' },
+];
+
+export default skills;
