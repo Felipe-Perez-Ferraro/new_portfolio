43624c7e4bc68c5cf57e505a088a3bbb341588a0
Felipe <fperezferraro@gmail.com>
2024-01-13 19:20:49 -0300

    Fixed popup error

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -34,9 +34,7 @@ function Projects({ design }) {
   return (
     <AnimatePresence>
       <motion.section
-        className={`flex flex-col gap-6 justify-center items-center h-screen ${
-          colorDesigns[design].background
-        } ${open ? 'backdrop-blur-sm' : null}`}
+        className={`flex flex-col gap-6 justify-center items-center h-screen ${colorDesigns[design].background} `}
         initial={{ x: -300, opacity: 0 }}
         animate={{ x: 0, opacity: 1 }}
         exit={{ x: -300, opacity: 0 }}
@@ -45,91 +43,99 @@ function Projects({ design }) {
           opacity: { duration: 0.2 },
         }}
       >
-        <Comeback design={colorDesigns[design]} />
-        {filterData && filterData.length > 0 && (
-          <article className="flex flex-col max-w-xs md:max-w-md lg:max-w-2xl xl:max-w-3xl mx-auto gap-4">
-            <h2
-              className={`text-4xl text-center uppercase font-black ${colorDesigns[design].title}`}
-            >
-              Projects
-            </h2>
-            <div className="flex flex-col gap-2 lg:flex-row">
-              {filterData.map((pro, idx) => (
-                <div
-                  className={`flex flex-col gap-2 border ${colorDesigns[design].border} rounded p-1 md:w-[400px]`}
-                  key={pro.id}
-                  id={pro.id}
+        {open ? (
+          filterData[popup.index] && filterData[popup.index].id === popup.id ? (
+            <AnimatePresence>
+              {open && (
+                <Popup
+                  key={filterData[popup.index].id}
+                  index={filterData[popup.index]}
+                  close={handleCloseClick}
+                  design={colorDesigns[design]}
+                />
+              )}
+            </AnimatePresence>
+          ) : null
+        ) : (
+          <>
+            <Comeback design={colorDesigns[design]} />
+            {filterData && filterData.length > 0 && (
+              <article className="flex flex-col max-w-xs md:max-w-md lg:max-w-2xl xl:max-w-3xl mx-auto gap-4">
+                <h2
+                  className={`text-4xl text-center uppercase font-black ${colorDesigns[design].title}`}
                 >
-                  <figure>
-                    <img
-                      src={pro.img}
-                      alt="project image"
-                      className="rounded w-full h-40"
-                    />
-                  </figure>
-                  <h2
-                    className={`text-lg font-bold text-center ${colorDesigns[design].subtitle}`}
-                  >
-                    {pro.name}
-                  </h2>
-                  {pro.pairProgramming ? (
-                    <p className={`font-semibold ${colorDesigns[design].text}`}>
-                      Pair Programming: Yes
-                    </p>
-                  ) : (
-                    <p className={`font-semibold ${colorDesigns[design].text}`}>
-                      Pair Programming: No
-                    </p>
-                  )}
-                  <ul className="flex gap-3">
-                    {pro.tech.map((t, idx) => (
-                      <li
-                        className={`font-semibold ${colorDesigns[design].linkBg} ${colorDesigns[design].text} px-1 rounded-lg`}
-                        key={idx}
-                      >
-                        {t}
-                      </li>
-                    ))}
-                  </ul>
-                  <div className="flex justify-center gap-4 mt-2">
-                    <button
-                      className={`${colorDesigns[design].paragraph} font-bold uppercase`}
-                      type="button"
-                      onClick={() => handleOpenClick(pro.id, idx)}
+                  Projects
+                </h2>
+                <div className="flex flex-col gap-2 lg:flex-row">
+                  {filterData.map((pro, idx) => (
+                    <div
+                      className={`flex flex-col gap-2 border ${colorDesigns[design].border} rounded p-1 md:w-[400px]`}
+                      key={pro.id}
+                      id={pro.id}
                     >
-                      Open
-                    </button>
-                  </div>
+                      <figure>
+                        <img
+                          src={pro.img}
+                          alt="project image"
+                          className="rounded w-full h-40"
+                        />
+                      </figure>
+                      <h2
+                        className={`text-lg font-bold text-center ${colorDesigns[design].subtitle}`}
+                      >
+                        {pro.name}
+                      </h2>
+                      {pro.pairProgramming ? (
+                        <p
+                          className={`font-semibold ${colorDesigns[design].text}`}
+                        >
+                          Pair Programming: Yes
+                        </p>
+                      ) : (
+                        <p
+                          className={`font-semibold ${colorDesigns[design].text}`}
+                        >
+                          Pair Programming: No
+                        </p>
+                      )}
+                      <ul className="flex gap-3">
+                        {pro.tech.map((t, idx) => (
+                          <li
+                            className={`font-semibold ${colorDesigns[design].linkBg} ${colorDesigns[design].text} px-1 rounded-lg`}
+                            key={idx}
+                          >
+                            {t}
+                          </li>
+                        ))}
+                      </ul>
+                      <div className="flex justify-center gap-4 mt-2">
+                        <button
+                          className={`${colorDesigns[design].paragraph} font-bold uppercase`}
+                          type="button"
+                          onClick={() => handleOpenClick(pro.id, idx)}
+                        >
+                          Open
+                        </button>
+                      </div>
+                    </div>
+                  ))}
                 </div>
-              ))}
-            </div>
-            {filterData[popup.index] &&
-            filterData[popup.index].id === popup.id ? (
-              <AnimatePresence>
-                {open && (
-                  <Popup
-                    key={filterData[popup.index].id}
-                    index={filterData[popup.index]}
-                    close={handleCloseClick}
-                    design={colorDesigns[design]}
-                  />
-                )}
-              </AnimatePresence>
-            ) : null}
-          </article>
+              </article>
+            )}
+            <ReactPaginate
+              className="flex gap-5"
+              activeClassName="active"
+              pageClassName={`${colorDesigns[design].text}`}
+              onPageChange={(e) => setPage(e.selected)}
+              pageCount={Math.ceil(projects.length / n)}
+              breakLabel="..."
+              previousLabel={leftArrow}
+              previousClassName={`${colorDesigns[design].paragraph}`}
+              nextLabel={rightArrow}
+              nextClassName={`${colorDesigns[design].paragraph}`}
+            />
+          </>
         )}
-        <ReactPaginate
-          className="flex gap-5"
-          activeClassName="active"
-          pageClassName={`${colorDesigns[design].text}`}
-          onPageChange={(e) => setPage(e.selected)}
-          pageCount={Math.ceil(projects.length / n)}
-          breakLabel="..."
-          previousLabel={leftArrow}
-          previousClassName={`${colorDesigns[design].paragraph}`}
-          nextLabel={rightArrow}
-          nextClassName={`${colorDesigns[design].paragraph}`}
-        />
       </motion.section>
     </AnimatePresence>
   );
